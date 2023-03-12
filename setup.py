3794da4c261ca4171a06a71611a425cc922f7fae
Gabriel Pastorello <57769272+GabrielPastorello@users.noreply.github.com>
2023-03-12 20:56:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
             name='nbb_api',
-            version='0.1.0',
-            description='Python module for easy access to Brazilian basketball data (NBB (Novo Basquete Brasil), Liga Ouro and LDB)',
+            version='0.1.1',
+            description='Python package for easy access to Brazilian basketball data (NBB (Novo Basquete Brasil), Liga Ouro and LDB)',
             long_description=long_description,
             long_description_content_type="text/markdown",
             url='https://github.com/GabrielPastorello/nbb_api',
@@ -15,12 +15,13 @@ setuptools.setup(
             license='MIT',
             packages=setuptools.find_packages(),
             keywords=['nbb','novo basquete brasil','scraper','basketball',
-                      'international basketball','brazil'],
+                      'international basketball','brazil','ldb','liga-ouro'],
             python_requires=">=3.6",
             install_requires=['pandas==1.5.3',
                               'numpy==1.24.1',
                               'python-dateutil==2.8.2',
-                              'pytz==2022.7'
+                              'pytz==2022.7',
+                              'lxml==4.9.2'
                               ],
             classifiers=[
                 "Programming Language :: Python :: 3",
