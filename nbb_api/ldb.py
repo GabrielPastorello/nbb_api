91134a193fa315ebd317d83565e4ec5467083c07
Gabriel Pastorello <57769272+GabrielPastorello@users.noreply.github.com>
2023-03-14 12:02:12 +0100

    Added get_placares

--- nbb_api/ldb.py
+++ nbb_api/ldb.py
@@ -84,3 +84,60 @@ def get_stats(season, fase, categ, tipo='avg', quem='athletes', sofrido=False):
     
     return df
 
+def get_placares(season, fase):
+    
+    if str(season) not in seasons:
+        raise ValueError(str(season)+' não é um valor válido. Tente um de: "'+'", "'.join(seasons)+'".')
+    
+    if fase not in fases:
+        raise ValueError(str(fase)+' não é um valor válido. Tente um de: "'+'", "'.join(fases)+'".')
+    
+    season2 = season_dict[str(season)]
+    
+    fase = fase_dict[fase]
+    
+    if fase!='%5B%5D=1&phase%5B%5D=2&phase%5B%5D=3&phase%5B%5D=4': # total
+        url = 'https://lnb.com.br/liga-ouro/tabela-de-jogos/?season%5B%5D='+season2+'&phase'+fase
+    else:
+        url = 'https://lnb.com.br/liga-ouro/tabela-de-jogos/?season%5B%5D='+season2
+    
+    df = pd.read_html(url)[0]
+    
+    try:
+        df = df.drop(columns=['#','CASA','Unnamed: 15','GINÁSIO','RODADA'])
+    except:
+        df = df.drop(columns=['#','CASA','Unnamed: 14','GINÁSIO','RODADA'])
+        
+    df = df.dropna(how='all', axis=1)
+    
+    df['DATA'] = pd.to_datetime(df['DATA'], format='%d/%m/%Y  %H:%M')
+    
+    df = df.rename(columns={'TRANSMISSÃO':'GINASIO',
+                            'FASE':'RODADA',
+                           'CAMPEONATO':'FASE',
+                           'Unnamed: 3':'EQUIPE CASA',
+                           'Unnamed: 7':'EQUIPE VISITANTE'})
+
+    df['Unnamed: 5'] = df['Unnamed: 5'].str.replace('  VER RELATÓRIO','')
+    df['PLACAR CASA'] = df['Unnamed: 5'].str[:2]
+    df['PLACAR VISITANTE'] = df['Unnamed: 5'].str[-2:]
+    
+    df['PLACAR CASA'] = df['PLACAR CASA'].apply(lambda x: np.nan if 'X' in x else x)
+    df['PLACAR VISITANTE'] = df['PLACAR VISITANTE'].apply(lambda x: np.nan if 'X' in x else x)
+    
+    df = df.drop(columns=['Unnamed: 5'])
+    
+    df['VENCEDOR'] = np.where(df['PLACAR CASA']>df['PLACAR VISITANTE'],df['EQUIPE CASA'],df['EQUIPE VISITANTE'])
+    
+    df['VENCEDOR'] = np.where(df['PLACAR CASA']==np.nan, np.nan, df['VENCEDOR'])
+    
+    df['TEMPORADA'] = season
+    
+    try:
+        df = df[['DATA','EQUIPE CASA','PLACAR CASA','PLACAR VISITANTE','EQUIPE VISITANTE',
+                'VENCEDOR','RODADA','FASE','GINASIO','TEMPORADA']]
+    except:
+        df = df[['DATA','EQUIPE CASA','PLACAR CASA','PLACAR VISITANTE','EQUIPE VISITANTE',
+                'VENCEDOR','RODADA','FASE','TEMPORADA']]    
+    
+    return df
